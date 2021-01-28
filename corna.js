4be20f9716739bb6fa4a506c1ba0882dbbddb058
Fabi <8header8@googlemail.com>
2021-01-28 17:50:13 +0100

    fixed if data from api is too old

--- corna.js
+++ corna.js
@@ -55,14 +55,14 @@ class CoronaWidget {
       'Wolfschlugen': 43,
     };
   }
-  
+
   async run() {
-      let widget = await this.createWidget()
-      if (!config.runsInWidget) {
-        await widget.presentSmall()
-      }
-      Script.setWidget(widget)
-      Script.complete()
+    let widget = await this.createWidget()
+    if (!config.runsInWidget) {
+      await widget.presentSmall()
+    }
+    Script.setWidget(widget)
+    Script.complete()
   }
 
   async createWidget() {
@@ -75,83 +75,86 @@ class CoronaWidget {
     textStack.setPadding(4, 8, 0, 8)
     textStack.layoutVertically()
     textStack.topAlignContent()
-       
-    if(data.error) {
+
+    if (data.error) {
       let errorText = textStack.addText(data.error.toUpperCase())
       textStack.setPadding(14, 14, 14, 14)
       errorText.font = Font.mediumSystemFont(13)
     } else {
-      list.refreshAfterDate = new Date(Date.now() + 60*60*1000)
+      list.refreshAfterDate = new Date(Date.now() + 60 * 60 * 1000)
 
 
-    let headerText = textStack.addText("Coronazahlen \n" + data.cityName.toUpperCase())
-    headerText.textColor = Color.green()
-    headerText.font = Font.mediumSystemFont(12)
+      let headerText = textStack.addText("Coronazahlen \n" + data.cityName.toUpperCase())
+      headerText.textColor = Color.green()
+      headerText.font = Font.mediumSystemFont(12)
 
-    let text1 = textStack.addText("Infizierte: " + data.infectedPersons)
-    text1.textColor = Color.dynamic(Color.black(), Color.white())
-    text1.font = Font.mediumSystemFont(10)
+      let text1 = textStack.addText("Infizierte: " + data.infectedPersons)
+      text1.textColor = Color.dynamic(Color.black(), Color.white())
+      text1.font = Font.mediumSystemFont(10)
 
-    let text2 = textStack.addText("Neuinfizierte: " + data.inf_neu)
-    text2.textColor = Color.dynamic(Color.black(), Color.white())
-    text2.font = Font.mediumSystemFont(10)
+      let text2 = textStack.addText("Neuinfizierte: " + data.inf_neu)
+      text2.textColor = Color.dynamic(Color.black(), Color.white())
+      text2.font = Font.mediumSystemFont(10)
 
-    let text3 = textStack.addText("Bevölkerung Prozent: " + data.residentsPercent)
-    text3.textColor = Color.dynamic(Color.black(), Color.white())
-    text3.font = Font.mediumSystemFont(10)
+      let text3 = textStack.addText("Bevölkerung Prozent: " + data.residentsPercent)
+      text3.textColor = Color.dynamic(Color.black(), Color.white())
+      text3.font = Font.mediumSystemFont(10)
 
-    let text4 = textStack.addText("Infizierte gesamt: " + data.infTotal)
-    text4.textColor = Color.dynamic(Color.black(), Color.white())
-    text4.font = Font.mediumSystemFont(10)
+      let text4 = textStack.addText("Infizierte gesamt: " + data.infTotal)
+      text4.textColor = Color.dynamic(Color.black(), Color.white())
+      text4.font = Font.mediumSystemFont(10)
 
-    let text5 = textStack.addText("Tote: " + data.deaths)
-    text5.textColor = Color.dynamic(Color.black(), Color.white())
-    text5.font = Font.mediumSystemFont(10)
+      let text5 = textStack.addText("Tote: " + data.deaths)
+      text5.textColor = Color.dynamic(Color.black(), Color.white())
+      text5.font = Font.mediumSystemFont(10)
 
-    let text6 = textStack.addText("Neue Tote: " + data.tod_neu)
-    text6.textColor = Color.dynamic(Color.black(), Color.white())
-    text6.font = Font.mediumSystemFont(10)
+      let text6 = textStack.addText("Neue Tote: " + data.tod_neu)
+      text6.textColor = Color.dynamic(Color.black(), Color.white())
+      text6.font = Font.mediumSystemFont(10)
 
-    let test7 = textStack.addText("Einwohner: " + data.residents)//+"\nDatum: "+ data.datum)
-    test7.textColor = Color.white()
-    test7.font = Font.mediumSystemFont(8)
+      let test7 = textStack.addText("Einwohner: " + data.residents)//+"\nDatum: "+ data.datum)
+      test7.textColor = Color.white()
+      test7.font = Font.mediumSystemFont(8)
 
-    let test8 = textStack.addText("Aktualisiert: " + data.lastupDated)
-    test8.textColor = Color.white()
-    test8.font = Font.mediumSystemFont(6)
+      let test8 = textStack.addText("Aktualisiert: " + data.lastupDated)
+      test8.textColor = Color.white()
+      test8.font = Font.mediumSystemFont(6)
 
-//Was ist das?
-/*
-    let test6 = textStack.addText("gen: " + data.gen)
-    test6.textColor = Color.white()
-    test6.font = Font.mediumSystemFont(10)
-    */
+      //Was ist das?
+      /*
+          let test6 = textStack.addText("gen: " + data.gen)
+          test6.textColor = Color.white()
+          test6.font = Font.mediumSystemFont(10)
+          */
     }
     return list
   }
-  
+
   async getData() {
-     try {
-      var d = new Date();
-      var hours = d.getHours().toString();
-      if(hours.toString().length == 1) {
-        hours = '0'+hours;
-      }
-    var minute = d.getMinutes().toString();
-    if(minute.toString().length == 1) {
-      minute = '0'+minute;
+
+    var d = new Date();
+    var hours = d.getHours().toString();
+    if (hours.toString().length == 1) {
+      hours = '0' + hours;
     }
-    var lastupDated = hours+":"+minute
+    var minute = d.getMinutes().toString();
+    if (minute.toString().length == 1) {
+      minute = '0' + minute;
+    }
+    var lastupDated = hours + ":" + minute
+    var day = (d.getDate() - 1);
+    var month = (d.getMonth() + 1).toString();
+    if (month.toString().length == 1) {
+      month = '0' + month;
+    }
+    var year = d.getFullYear().toString();
 
 
-      var day = (d.getDate()-1).toString();
-      var month = (d.getMonth()+1).toString();
-        if(month.toString().length == 1) {
-             month = '0'+month;
-        }
-      var year = d.getFullYear().toString();
-      var dateformat = year+month+day;
-      let apiUrl = "https://services2.arcgis.com/mL26ZKdlhFJH9AoM/arcgis/rest/services/es_corona/FeatureServer/0/query?f=json&where=dat_text%3D"+dateformat+"&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=NAMGEM%20asc&resultOffset=0&resultRecordCount=50&resultType=standard&cacheHint=true"
+
+    try {
+
+      var dateformat = year + month + day.toString();
+      let apiUrl = "https://services2.arcgis.com/mL26ZKdlhFJH9AoM/arcgis/rest/services/es_corona/FeatureServer/0/query?f=json&where=dat_text%3D" + dateformat + "&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=NAMGEM%20asc&resultOffset=0&resultRecordCount=50&resultType=standard&cacheHint=true"
 
       const r = new Request(apiUrl)
       let resp = await r.loadJSON()
@@ -188,7 +191,7 @@ class CoronaWidget {
       if (city in this.cities) {
         let number1 = this.cities[city]
         attr = features[number1].attributes
-      }else{
+      } else {
         return { error: "Stadt nicht gefunden. Bitte Name bei Parametern ändern" };
       }
 
@@ -199,14 +202,14 @@ class CoronaWidget {
       let residentsPercent = attr.per_qua_p.toString()
       let residents = attr.einw.toString()
       let infTotal = attr.inf_ges.toString()
-      let inzidenz  = attr.inzidenz.toString().substr(0,4)
+      let inzidenz = attr.inzidenz.toString().substr(0, 4)
       let gen = attr.gen_p.toString()
       let proz_gem = attr.proz_gem.toString()
       let tod_neu = attr.tod_neu.toString()
       let inf_neu = attr.inf_neu.toString()
       let datum = attr.dat_zahl.toString()
       //console.log(cityName)
-      return{
+      return {
         cityName: cityName,
         infectedPersons: infectedPersons,
         residentsPercent: residentsPercent,
@@ -223,12 +226,64 @@ class CoronaWidget {
 
 
       };
-    } catch(e) {
-      return { error: "Error getting data" };
+    } catch (e) {
+
+      try {
+
+        var dateformat = year + month + (day-1).toString();
+        let apiUrl = "https://services2.arcgis.com/mL26ZKdlhFJH9AoM/arcgis/rest/services/es_corona/FeatureServer/0/query?f=json&where=dat_text%3D" + dateformat + "&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=NAMGEM%20asc&resultOffset=0&resultRecordCount=50&resultType=standard&cacheHint=true"
+
+        const r = new Request(apiUrl)
+        let resp = await r.loadJSON()
+        let features = resp.features
+
+
+        var attr = features[0].attributes
+        let city = "Plochingen"
+        city = param.toString()
+
+        if (city in this.cities) {
+          let number1 = this.cities[city]
+          attr = features[number1].attributes
+        } else {
+          return { error: "Stadt nicht gefunden. Bitte Name bei Parametern ändern" };
+        }
+
+
+        let cityName = attr.NAMGEM
+        let infectedPersons = attr.pers_qua.toString()
+        let deaths = attr.tod_ins.toString()
+        let residentsPercent = attr.per_qua_p.toString()
+        let residents = attr.einw.toString()
+        let infTotal = attr.inf_ges.toString()
+        let inzidenz = attr.inzidenz.toString().substr(0, 4)
+        let gen = attr.gen_p.toString()
+        let proz_gem = attr.proz_gem.toString()
+        let tod_neu = attr.tod_neu.toString()
+        let inf_neu = attr.inf_neu.toString()
+        let datum = attr.dat_zahl.toString()
+        return {
+          cityName: cityName,
+          infectedPersons: infectedPersons,
+          residentsPercent: residentsPercent,
+          infTotal: infTotal,
+          inzidenz: inzidenz,
+          gen: gen,
+          residents: residents,
+          proz_gem: proz_gem,
+          tod_neu: tod_neu,
+          inf_neu: inf_neu,
+          datum: datum,
+          lastupDated: lastupDated,
+          deaths: deaths
+        };
+      } catch (e) {
+        return { error: "Error getting data" };
+      }
     }
   }
-  
-  
+
+
 }
 
 await new CoronaWidget().run();
